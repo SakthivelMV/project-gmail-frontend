af1beef85c4941a4769a8bc325588001999552dc
sakthivelm554@gmail.com <sakthivelm554@gmail.com>
2023-12-08 12:36:37 +0530

    dec update

---
+++ build/static/js/848.75d24f32.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkclient=self.webpackChunkclient||[]).push([[848],{7848:function(e,n,r){r.r(n);var l=r(4554),t=r(890),a=r(7689),i=r(184);n.default=function(){var e=(0,a.lk)();return console.log(e),(0,i.jsx)(l.Z,{style:{marginLeft:250},children:(0,i.jsx)(t.Z,{variant:"h4",children:"There was error loading this page"})})}}}]);
+//# sourceMappingURL=848.75d24f32.chunk.js.map
